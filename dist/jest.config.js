6702901c7953aaf8dd5854806711b081e24fec7f
Felipe5215 <felipe.eleotero@gmail.com>
2022-07-24 21:05:12 -0300

    removendo arquivos desnecessarios subidos por engano

--- dist/jest.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-module.exports = {
-    clearMocks: true,
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    setupFilesAfterEnv: ['<rootDir>/singleton.ts']
-};
-//# sourceMappingURL=jest.config.js.map
